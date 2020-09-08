bc6aca9e569a56fe9f64970dbc401101dbbcb21b
Sebastian Meier <contact@sebastianmeier.eu>
2020-09-08 18:46:15 +0200

    ckan tests passed

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv');
 const pg = require('pg');
+const {packageList, packageShow} = require('../build/src/ckan/index')
 
 // get environmental variables
 dotenv.config();
@@ -12,15 +13,37 @@ const client = new pg.Client({
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
 });
-client.connect();
 
-test('check if test database is available', () => {
-  client.query('SELECT version();')
+test('check if test database is available', async () => {
+  client.connect();
+  await client.query('SELECT version() AS v;')
     .then((result) => {
-      expect(result.rows[0]).toBeInstanceOf(String);
+      expect(result).toHaveProperty('rows');
+      expect(result.rows.length).toBeGreaterThan(0);
+      expect(typeof result.rows[0].v).toBe('string');
+    });
+  client.end();
+});
+
+// Run tests for all connected ckan portals
+
+test('get list of packages (using ckan.govdata.de)', async () => {
+  await packageList('ckan.govdata.de')
+    .then((ckanPackageList) => {
+      expect(ckanPackageList).toHaveProperty('result');
+      expect(ckanPackageList.result.length).toBeGreaterThan(0);
+      expect(typeof ckanPackageList.result[0]).toBe('string');
     });
 });
 
-client.end();
+test('get an example package (using ckan.govdata.de)', async () => {
+  await packageList('ckan.govdata.de')
+    .then((ckanPackageList) => packageShow('ckan.govdata.de', ckanPackageList.result[0]))
+    .then((ckanPackage) => {
+      expect(ckanPackage).toHaveProperty('result');
+      // if the returned packages have another structure than expected, one of the latter tests will notice this
+      expect(ckanPackage.result).toHaveProperty('id');
+    });
+});
 
 
