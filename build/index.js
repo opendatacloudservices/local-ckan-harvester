24774bf6226b888f27209872852b68712701d97a
Sebastian Meier <contact@sebastianmeier.eu>
2020-09-12 02:35:34 +0200

    express integration through local-microservice

--- build/index.js
+++ build/index.js
@@ -6,21 +6,54 @@ const dotenv = require("dotenv");
 const pg_1 = require("pg");
 // get environmental variables
 dotenv.config();
+const local_microservice_1 = require("local-microservice");
 // connect to postgres (via env vars params)
 const client = new pg_1.Client();
 client.connect();
-// import packages from a ckan instance
-const domain = process.argv[2];
-const prefix = process.argv[3];
-index_1.packageList(domain)
-    .then(async (list) => {
-    for (let i = 0; i < list.result.length; i += 1) {
-        await index_1.packageShow(domain, list.result[i]).then(async (ckanPackage) => {
-            return index_2.processPackage(client, prefix, ckanPackage);
-        });
-    }
-})
-    .catch(err => {
-    throw err;
+local_microservice_1.api.get('/process/:prefix/:domain', (req, res) => {
+    // TODO: check if prefix and domain exist > store prefix and domain in a database and only send domain?
+    index_1.packageList(req.params.domain)
+        .then(async (list) => {
+        for (let i = 0; i < list.result.length; i += 1) {
+            await index_1.packageShow(req.params.domain, list.result[i]).then(async (ckanPackage) => {
+                return index_2.processPackage(client, req.params.prefix, ckanPackage);
+            });
+        }
+        res.status(200).json({ message: 'Process completed' });
+    })
+        .catch(err => {
+        res.status(500).json({ error: err.message });
+        throw err;
+    });
+});
+local_microservice_1.api.get('/init/:prefix', (req, res) => {
+    index_2.initTables(client, req.params.prefix)
+        .then(() => {
+        res.status(200).json({ message: 'Init completed' });
+    })
+        .catch(err => {
+        res.status(500).json({ error: err.message });
+        throw err;
+    });
+});
+local_microservice_1.api.get('/reset/:prefix', (req, res) => {
+    index_2.resetTables(client, process.argv[2])
+        .then(() => {
+        res.status(200).json({ message: 'Reset completed' });
+    })
+        .catch(err => {
+        res.status(500).json({ error: err.message });
+        throw err;
+    });
+});
+local_microservice_1.api.get('/drop/:prefix', (req, res) => {
+    index_2.dropTables(client, process.argv[2])
+        .then(() => {
+        res.status(200).json({ message: 'Drop completed' });
+    })
+        .catch(err => {
+        res.status(500).json({ error: err.message });
+        throw err;
+    });
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -4,31 +4,91 @@ import {
   CkanPackageList,
   CkanPackage,
 } from './ckan/index';
-import {processPackage} from './postgres/index';
+import {
+  processPackage,
+  initTables,
+  resetTables,
+  dropTables,
+} from './postgres/index';
 import * as dotenv from 'dotenv';
 import {Client} from 'pg';
 
 // get environmental variables
 dotenv.config();
 
+import {api, startTransaction, startSpan} from 'local-microservice';
+
 // connect to postgres (via env vars params)
 const client = new Client();
 client.connect();
 
-// import packages from a ckan instance
-const domain = process.argv[2];
-const prefix = process.argv[3];
+api.get('/process/:prefix/:domain', (req, res) => {
+  const transaction = startTransaction('process', {
 
-packageList(domain)
-  .then(async (list: CkanPackageList) => {
-    for (let i = 0; i < list.result.length; i += 1) {
-      await packageShow(domain, list.result[i]).then(
-        async (ckanPackage: CkanPackage) => {
-          return processPackage(client, prefix, ckanPackage);
-        }
-      );
-    }
-  })
-  .catch(err => {
-    throw err;
   });
+
+  // TODO: check if prefix and domain exist > store prefix and domain in a database and only send domain?
+  packageList(req.params.domain)
+    .then(async (list: CkanPackageList) => {
+      for (let i = 0; i < list.result.length; i += 1) {
+        const span = startSpan('receiveAndProcessPackage');
+        await packageShow(req.params.domain, list.result[i]).then(
+          async (ckanPackage: CkanPackage) => {
+            return processPackage(client, req.params.prefix, ckanPackage);
+          }
+        );
+        if (span) {
+          // TODO, bundle in local-microservice
+          span.end();
+        }
+      }
+      res.status(200).json({message: 'Process completed'});
+      // TODO, bundle in local-microservice
+      if (transaction) {
+        transaction.result = 'success';
+        transaction.end();
+      }
+    })
+    .catch(err => {
+      res.status(500).json({error: err.message});
+      // TODO, bundle in local-microservice
+      if (transaction) {
+        transaction.result = 'error';
+        transaction.end();
+      }
+      throw err;
+    });
+});
+
+api.get('/init/:prefix', (req, res) => {
+  initTables(client, req.params.prefix)
+    .then(() => {
+      res.status(200).json({message: 'Init completed'});
+    })
+    .catch(err => {
+      res.status(500).json({error: err.message});
+      throw err;
+    });
+});
+
+api.get('/reset/:prefix', (req, res) => {
+  resetTables(client, process.argv[2])
+    .then(() => {
+      res.status(200).json({message: 'Reset completed'});
+    })
+    .catch(err => {
+      res.status(500).json({error: err.message});
+      throw err;
+    });
+});
+
+api.get('/drop/:prefix', (req, res) => {
+  dropTables(client, process.argv[2])
+    .then(() => {
+      res.status(200).json({message: 'Drop completed'});
+    })
+    .catch(err => {
+      res.status(500).json({error: err.message});
+      throw err;
+    });
+});
